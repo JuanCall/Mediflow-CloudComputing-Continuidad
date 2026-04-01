be3bafa9ed710102a2e8862f1313a522375cf2ca
JuanCall <juancallerosales19@gmail.com>
2026-04-01 08:15:18 -0500

    Init: Estructura base completa (frontend y backend)

---
+++ backend/firebaseAdmin.js
@@ -0,0 +1,8 @@
+const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccountKey.json'); // Descomentar cuando se tenga la key
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares globales
+app.use(cors());
+app.use(express.json());
+
+// Importación de rutas
+const authRoutes = require('./routes/authRoutes');
+
+// Uso de rutas
+app.use('/api/auth', authRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('API de Mediflow funcionando correctamente en la nube ☁️');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
